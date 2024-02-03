48dfed9f1a396a105f3cb1cd2cf0841ee899ad93
Samy <iallysamy@gmail.com>
2024-02-03 19:37:24 -0300

    Home com o card que contém a imagem!

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import styles from './Home.module.css'
 import BannerProjetoFinalReactPretaLab from '../../assets/BannerProjetoFinalReactPretaLab.png'
+import TWO_BLACK_POWER from '../../assets/TWO_BLACK_POWER.png'
 
 
 const Home = () => {
@@ -15,6 +16,10 @@ const Home = () => {
           <br/>
           <a class="link-formacao" href="https://www.canva.com/design/DAF7MCKi7jE/C1i7JzmO1Nn8sRqTpQttuA/view" target="_blank">Proposta do projeto com os requisitos obrigatórios e opcionais.</a>
         </p>
+        </div>
+      <div className={styles.motivoContainer}>
+        <h2 className={styles.h2}>Porque iniciei os estudos no Pretalab</h2>
+          <img src={TWO_BLACK_POWER}/>
       </div>
     </>
   )
