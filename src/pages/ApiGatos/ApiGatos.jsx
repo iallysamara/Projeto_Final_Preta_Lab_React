8fe165367c2d58fabb5bd0a2380a75e0a634659b
Samy <iallysamy@gmail.com>
2024-02-16 09:22:06 -0300

    Adicionei um estado para enquanto estiver carregando as imagens da API de gatinhos ele mostrar uma mensagem para o usuário

--- src/pages/ApiGatos/ApiGatos.jsx
+++ src/pages/ApiGatos/ApiGatos.jsx
@@ -4,15 +4,20 @@ import styles from './ApiGatos.module.css';
 
 const ApiGatos = () => {
     const [gatoImagens, setgatoImagens] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
-        axios.get('https://api.thecatapi.com/v1/images/search?limit=15')
+        axios.get('https://api.thecatapi.com/v1/images/search?limit=10')
         .then(response => {
             setgatoImagens(response.data);
+            setIsLoading(false);
         })
         .catch(error => {
             console.error('Erro ao obter imagens de gatos:', error);
+            setIsLoading(false);
         });
     }, []);
+
     return (
         <>
         <div className={styles.header}>
@@ -20,15 +25,21 @@ const ApiGatos = () => {
                 Prepare-se para um desfile de fofura felina! 😺✨
             </h1>
         </div>
-        <div className={styles.gatoCards}>
-            {gatoImagens.map(gato => (
-            <div key={gato.id} className={styles.card}>
-                <img src={gato.url} alt="Gato" />
+        {isLoading ? (
+            <div className={styles.loading}>
+                Carregando imagens de gatinhos...
             </div>
-        ))}
-        </div>
+        ) : (
+            <div className={styles.gatoCards}>
+                {gatoImagens.map(gato => (
+                    <div key={gato.id} className={styles.card}>
+                        <img src={gato.url} alt="Gato" />
+                    </div>
+                ))}
+            </div>
+        )}
         </>
-  );
+    );
 };
 
 export default ApiGatos;
